7134bad7383affc1feb2ccea0c259a79964be7c1
LynnL4 <lht856@foxmail.com>
2022-09-30 14:39:28 +0800

    tinyusb: set usb irq priority to max syscall.

--- cores/arduino/TinyUSB/Adafruit_TinyUSB_SAMD.cpp
+++ cores/arduino/TinyUSB/Adafruit_TinyUSB_SAMD.cpp
@@ -35,19 +35,23 @@ extern "C"
 {
 #if defined(__SAMD51__)
 
-void USB_0_Handler (void) { tud_int_handler(0); }
-void USB_1_Handler (void) { tud_int_handler(0); }
-void USB_2_Handler (void) { tud_int_handler(0); }
-void USB_3_Handler (void) { tud_int_handler(0); }
+  void USB_0_Handler(void)
+  {
+    tud_int_handler(0);
+  }
+  void USB_1_Handler(void) { tud_int_handler(0); }
+  void USB_2_Handler(void) { tud_int_handler(0); }
+  void USB_3_Handler(void) { tud_int_handler(0); }
 
 #else
 
-void USB_Handler(void) { tud_int_handler(0); }
+  void USB_Handler(void)
+  {
+    tud_int_handler(0);
+  }
 
 #endif
 } // extern C
-
-
 
 //--------------------------------------------------------------------+
 // MACRO TYPEDEF CONSTANT ENUM DECLARATION
@@ -64,7 +68,6 @@ extern "C" int serial1_printf(const char *__restrict format, ...)
   Serial1.write(buf);
   Serial1.flush();
   va_end(ap);
-
 }
 #endif
 
@@ -78,7 +81,7 @@ void Adafruit_TinyUSB_Core_init(void)
   serial1_printf("TinyUSB debugging with Serial1\n");
 #endif
 
-  USBDevice.addInterface( (Adafruit_USBD_Interface&) Serial);
+  USBDevice.addInterface((Adafruit_USBD_Interface &)Serial);
   USBDevice.setID(USB_VID, USB_PID);
   USBDevice.begin();
 
@@ -97,38 +100,45 @@ void Adafruit_TinyUSB_Core_touch1200(void)
 // Adafruit_USBD_Device platform dependent
 //--------------------------------------------------------------------+
 
-uint8_t Adafruit_USBD_Device::getSerialDescriptor(uint16_t* serial_str)
+uint8_t Adafruit_USBD_Device::getSerialDescriptor(uint16_t *serial_str)
 {
-  enum { SERIAL_BYTE_LEN = 16 };
+  enum
+  {
+    SERIAL_BYTE_LEN = 16
+  };
 
 #ifdef __SAMD51__
-  uint32_t* id_addresses[4] = {(uint32_t *) 0x008061FC, (uint32_t *) 0x00806010,
-                               (uint32_t *) 0x00806014, (uint32_t *) 0x00806018};
+  uint32_t *id_addresses[4] = {(uint32_t *)0x008061FC, (uint32_t *)0x00806010,
+                               (uint32_t *)0x00806014, (uint32_t *)0x00806018};
 #else // samd21
-  uint32_t* id_addresses[4] = {(uint32_t *) 0x0080A00C, (uint32_t *) 0x0080A040,
-                               (uint32_t *) 0x0080A044, (uint32_t *) 0x0080A048};
+  uint32_t *id_addresses[4] = {(uint32_t *)0x0080A00C, (uint32_t *)0x0080A040,
+                               (uint32_t *)0x0080A044, (uint32_t *)0x0080A048};
 
 #endif
 
   uint8_t raw_id[SERIAL_BYTE_LEN];
 
-  for (int i=0; i<4; i++) {
-      for (int k=0; k<4; k++) {
-          raw_id[4 * i + (3 - k)] = (*(id_addresses[i]) >> k * 8) & 0xff;
-      }
+  for (int i = 0; i < 4; i++)
+  {
+    for (int k = 0; k < 4; k++)
+    {
+      raw_id[4 * i + (3 - k)] = (*(id_addresses[i]) >> k * 8) & 0xff;
+    }
   }
 
   static const char nibble_to_hex[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
-  for (unsigned int i = 0; i < sizeof(raw_id); i++) {
-    for (int j = 0; j < 2; j++) {
+  for (unsigned int i = 0; i < sizeof(raw_id); i++)
+  {
+    for (int j = 0; j < 2; j++)
+    {
       uint8_t nibble = (raw_id[i] >> (j * 4)) & 0xf;
       // Strings are UTF-16-LE encoded.
       serial_str[i * 2 + (1 - j)] = nibble_to_hex[nibble];
     }
   }
 
-  return sizeof(raw_id)*2;
+  return sizeof(raw_id) * 2;
 }
 
 //--------------------------------------------------------------------+
@@ -139,56 +149,64 @@ uint8_t Adafruit_USBD_Device::getSerialDescriptor(uint16_t* serial_str)
 static void usb_hardware_init(void)
 {
 #ifdef PIN_LED_TXL
-//	txLEDPulse = 0;
-	pinMode(PIN_LED_TXL, OUTPUT);
-	digitalWrite(PIN_LED_TXL, HIGH);
+  //	txLEDPulse = 0;
+  pinMode(PIN_LED_TXL, OUTPUT);
+  digitalWrite(PIN_LED_TXL, HIGH);
 #endif
 
 #ifdef PIN_LED_RXL
-//	rxLEDPulse = 0;
-	pinMode(PIN_LED_RXL, OUTPUT);
-	digitalWrite(PIN_LED_RXL, HIGH);
+  //	rxLEDPulse = 0;
+  pinMode(PIN_LED_RXL, OUTPUT);
+  digitalWrite(PIN_LED_RXL, HIGH);
 #endif
 
-	/* Enable USB clock */
+  /* Enable USB clock */
 #if defined(__SAMD51__)
-	MCLK->APBBMASK.reg |= MCLK_APBBMASK_USB;
-	MCLK->AHBMASK.reg |= MCLK_AHBMASK_USB;
+  MCLK->APBBMASK.reg |= MCLK_APBBMASK_USB;
+  MCLK->AHBMASK.reg |= MCLK_AHBMASK_USB;
 
-	// Set up the USB DP/DN pins
-	PORT->Group[0].PINCFG[PIN_PA24H_USB_DM].bit.PMUXEN = 1;
-	PORT->Group[0].PMUX[PIN_PA24H_USB_DM/2].reg &= ~(0xF << (4 * (PIN_PA24H_USB_DM & 0x01u)));
-	PORT->Group[0].PMUX[PIN_PA24H_USB_DM/2].reg |= MUX_PA24H_USB_DM << (4 * (PIN_PA24H_USB_DM & 0x01u));
-	PORT->Group[0].PINCFG[PIN_PA25H_USB_DP].bit.PMUXEN = 1;
-	PORT->Group[0].PMUX[PIN_PA25H_USB_DP/2].reg &= ~(0xF << (4 * (PIN_PA25H_USB_DP & 0x01u)));
-	PORT->Group[0].PMUX[PIN_PA25H_USB_DP/2].reg |= MUX_PA25H_USB_DP << (4 * (PIN_PA25H_USB_DP & 0x01u));
+  // Set up the USB DP/DN pins
+  PORT->Group[0].PINCFG[PIN_PA24H_USB_DM].bit.PMUXEN = 1;
+  PORT->Group[0].PMUX[PIN_PA24H_USB_DM / 2].reg &= ~(0xF << (4 * (PIN_PA24H_USB_DM & 0x01u)));
+  PORT->Group[0].PMUX[PIN_PA24H_USB_DM / 2].reg |= MUX_PA24H_USB_DM << (4 * (PIN_PA24H_USB_DM & 0x01u));
+  PORT->Group[0].PINCFG[PIN_PA25H_USB_DP].bit.PMUXEN = 1;
+  PORT->Group[0].PMUX[PIN_PA25H_USB_DP / 2].reg &= ~(0xF << (4 * (PIN_PA25H_USB_DP & 0x01u)));
+  PORT->Group[0].PMUX[PIN_PA25H_USB_DP / 2].reg |= MUX_PA25H_USB_DP << (4 * (PIN_PA25H_USB_DP & 0x01u));
 
+  GCLK->PCHCTRL[USB_GCLK_ID].reg = GCLK_PCHCTRL_GEN_GCLK1_Val | (1 << GCLK_PCHCTRL_CHEN_Pos);
 
-	GCLK->PCHCTRL[USB_GCLK_ID].reg = GCLK_PCHCTRL_GEN_GCLK1_Val | (1 << GCLK_PCHCTRL_CHEN_Pos);
-
-	NVIC_SetPriority(USB_0_IRQn, 2UL);
-	NVIC_SetPriority(USB_1_IRQn, 2UL);
-	NVIC_SetPriority(USB_2_IRQn, 2UL);
-	NVIC_SetPriority(USB_3_IRQn, 2UL);
+#if CFG_TUSB_OS == OPT_OS_FREERTOS
+  // If freeRTOS is used, IRQ priority is limit by max syscall ( smaller is higher )
+  NVIC_SetPriority(USB_0_IRQn, configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY);
+  NVIC_SetPriority(USB_1_IRQn, configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY);
+  NVIC_SetPriority(USB_2_IRQn, configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY);
+  NVIC_SetPriority(USB_3_IRQn, configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY);
 #else
-	PM->APBBMASK.reg |= PM_APBBMASK_USB;
+  NVIC_SetPriority(USB_0_IRQn, 0);
+  NVIC_SetPriority(USB_1_IRQn, 0);
+  NVIC_SetPriority(USB_2_IRQn, 0);
+  NVIC_SetPriority(USB_3_IRQn, 0);
+#endif
 
-	// Set up the USB DP/DN pins
-	PORT->Group[0].PINCFG[PIN_PA24G_USB_DM].bit.PMUXEN = 1;
-	PORT->Group[0].PMUX[PIN_PA24G_USB_DM/2].reg &= ~(0xF << (4 * (PIN_PA24G_USB_DM & 0x01u)));
-	PORT->Group[0].PMUX[PIN_PA24G_USB_DM/2].reg |= MUX_PA24G_USB_DM << (4 * (PIN_PA24G_USB_DM & 0x01u));
-	PORT->Group[0].PINCFG[PIN_PA25G_USB_DP].bit.PMUXEN = 1;
-	PORT->Group[0].PMUX[PIN_PA25G_USB_DP/2].reg &= ~(0xF << (4 * (PIN_PA25G_USB_DP & 0x01u)));
-	PORT->Group[0].PMUX[PIN_PA25G_USB_DP/2].reg |= MUX_PA25G_USB_DP << (4 * (PIN_PA25G_USB_DP & 0x01u));
+#else
+  PM->APBBMASK.reg |= PM_APBBMASK_USB;
 
-	// Put Generic Clock Generator 0 as source for Generic Clock Multiplexer 6 (USB reference)
-	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(6)     | // Generic Clock Multiplexer 6
-	GCLK_CLKCTRL_GEN_GCLK0 | // Generic Clock Generator 0 is source
-	GCLK_CLKCTRL_CLKEN;
-	while (GCLK->STATUS.bit.SYNCBUSY)
-	;
+  // Set up the USB DP/DN pins
+  PORT->Group[0].PINCFG[PIN_PA24G_USB_DM].bit.PMUXEN = 1;
+  PORT->Group[0].PMUX[PIN_PA24G_USB_DM / 2].reg &= ~(0xF << (4 * (PIN_PA24G_USB_DM & 0x01u)));
+  PORT->Group[0].PMUX[PIN_PA24G_USB_DM / 2].reg |= MUX_PA24G_USB_DM << (4 * (PIN_PA24G_USB_DM & 0x01u));
+  PORT->Group[0].PINCFG[PIN_PA25G_USB_DP].bit.PMUXEN = 1;
+  PORT->Group[0].PMUX[PIN_PA25G_USB_DP / 2].reg &= ~(0xF << (4 * (PIN_PA25G_USB_DP & 0x01u)));
+  PORT->Group[0].PMUX[PIN_PA25G_USB_DP / 2].reg |= MUX_PA25G_USB_DP << (4 * (PIN_PA25G_USB_DP & 0x01u));
 
-	NVIC_SetPriority((IRQn_Type) USB_IRQn, 2UL);
+  // Put Generic Clock Generator 0 as source for Generic Clock Multiplexer 6 (USB reference)
+  GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(6) |     // Generic Clock Multiplexer 6
+                      GCLK_CLKCTRL_GEN_GCLK0 | // Generic Clock Generator 0 is source
+                      GCLK_CLKCTRL_CLKEN;
+  while (GCLK->STATUS.bit.SYNCBUSY)
+    ;
+
+  NVIC_SetPriority((IRQn_Type)USB_IRQn, 2UL);
 #endif
 }
 
